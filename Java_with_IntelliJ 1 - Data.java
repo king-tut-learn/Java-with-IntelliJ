bb013e6bd576db502b1c574e3e0b78742392cacc
TERP <terposterp@yandex.com>
2020-06-14 10:38:25 -0400

    6/14/2020

---
+++ Java_with_IntelliJ 1 - Data.java
@@ -0,0 +1,24 @@
+package Tutorial;
+
+public class Getting_Started {
+
+    public static void main(String [] args)
+    {
+        //int, long, double, float, char, String
+        int x = 2;
+        long y = 3;
+        double z = 1.5;
+        float a = (float) 2.6;
+        char letter = 'q';
+        String word = "King Tut bla bla bla!!";
+
+        //outputting values of variables
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(letter);
+        System.out.println(word);
+    }
+
+}
